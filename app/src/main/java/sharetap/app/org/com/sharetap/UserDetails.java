4d37f01cb114bc5f163087285b1d1ff6d2874f29
RajNirmal <nirmal497@gmail.com>
2019-02-12 22:54:21 +0530

    Details page completed

---
+++ app/src/main/java/sharetap/app/org/com/sharetap/UserDetails.java
@@ -0,0 +1,80 @@
+package sharetap.app.org.com.sharetap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UserDetails extends AppCompatActivity {
+    TextView userName,userMail;
+    EditText fbDetails, instaDetails;
+    Button saveButton, backButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_user_details);
+        initViews();
+    }
+
+    private void initViews(){
+        userName = findViewById(R.id.user_name);
+        userMail = findViewById(R.id.user_mail);
+        fbDetails = findViewById(R.id.fb_profile_id);
+        instaDetails = findViewById(R.id.insta_profile_id);
+        saveButton = findViewById(R.id.save_user_details);
+        backButton = findViewById(R.id.open_main_page);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveAllDetails();
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainPage();
+            }
+        });
+        populateViews();
+    }
+
+    private void saveAllDetails(){
+        String fbNewProfileId = fbDetails.getText().toString();
+        String instaNewProfileId = instaDetails.getText().toString();
+        if(!fbNewProfileId.isEmpty()){
+            AppUtil.getUtilInstance().setFbId(UserDetails.this,fbNewProfileId);
+        }
+        if(!instaNewProfileId.isEmpty()){
+            AppUtil.getUtilInstance().setInstaId(UserDetails.this,instaNewProfileId);
+        }
+        Toast.makeText(UserDetails.this," Details saved successfully",Toast.LENGTH_SHORT).show();
+    }
+
+    private void populateViews(){
+        String userName = AppUtil.getUtilInstance().getUserName(UserDetails.this);
+        String userMail = AppUtil.getUtilInstance().getUserMail(UserDetails.this);
+        String fbUserName = AppUtil.getUtilInstance().getFBProfileID(UserDetails.this);
+        String instaProfileID = AppUtil.getUtilInstance().getInstaProfileID(UserDetails.this);
+        fbDetails.setText(fbUserName);
+        instaDetails.setText(instaProfileID);
+        this.userName.setText(userName);
+        this.userMail.setText(userMail);
+    }
+
+    private void openMainPage(){
+        Intent mainIntent = new Intent(UserDetails.this,MainActivity.class);
+        startActivity(mainIntent);
+    }
+}
